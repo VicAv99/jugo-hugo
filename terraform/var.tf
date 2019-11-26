f7561c2cae8b45291634d4dac1f9ed3de858dc8c
Victor Avila <victor.avila@venturplex.com>
2019-11-26 15:29:54 -0700

    tf

---
+++ terraform/var.tf
@@ -0,0 +1,14 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "root_domain_name" {
+  default = "aproofofconcept.com"
+}
+
+variable "domain_name" {
+  default = "jugo.aproofofconcept.com"
+}
